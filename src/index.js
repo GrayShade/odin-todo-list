49ddeeb991cac7ce6aebdb132c361bdb31447903
Gray <silicongray@gmail.com>
2025-03-06 10:09:53 +0500

    Implement expanding & collapsing of leftbar elements

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ class Main {
   static #taskUI = new TasksDisplay();
   start() {
 
+    this.setEventListeners();
+
     Main.#proj.createDefaultProject('default');
     // Main.#proj.createProject('custom Project');
     Main.#proj.updateProject(7, 'updated Project')
@@ -24,12 +26,12 @@ class Main {
     Main.#projUI.showSingleProject(0, Main.#proj.getAllProjects());
     Main.#taskUI.showAllTasks(0);
     Main.#taskUI.showSingleTask(Main.#task.getTask(0, 0));
-    
+
     Main.#task.deleteTask(0, 1);
     // Main.#task.createTask(0);
-    
+
     const date = format(new Date(2025, 1, 26), 'dd-MMM-yy');
-    
+
     const updatedTask = {
       taskId: 0,
       projId: 0,
@@ -41,6 +43,24 @@ class Main {
     Main.#task.updateTask(0, 0, updatedTask);
     Main.#task.getTask(0, 0);
   }
+
+  setEventListeners() {
+    const showHideDivArr = document.querySelectorAll('.showHide');
+    for (let showHideIdx = 0; showHideIdx <= showHideDivArr.length - 1; showHideIdx++) {
+      showHideDivArr[showHideIdx].addEventListener('click', (e) => {
+        Main.#ui.showHideDivs(e);
+        
+      });
+    }
+
+  }
+
+
+
+
+
+
+
 }
 
 let obj = new Main();
--- src/ui.js
+++ src/ui.js
@@ -1,3 +1,73 @@
-export class UI {
+import { compareAsc, format } from "date-fns";
+import { styles } from "./styles/styles.css"
+import { reset } from "./styles/reset.css"
+import { icons } from "./../node_modules/@vectopus/atlas-icons/style.css";
 
+export class UI {
+  showHideDivs(e) {
+    let showHideDiv;
+    let showHideParent;
+    if (e.target.parentElement.classList.contains('showHide')) {
+      showHideDiv = e.target.parentElement;
+      showHideParent = showHideDiv.parentElement;
+    } else if (e.target.parentElement.parentElement.classList.contains('showHide')) {
+      showHideDiv = e.target.parentElement.parentElement;
+      showHideParent = showHideDiv.parentElement;
+    }
+    this.showHideClicked(showHideParent);
+    this.hideSiblings(showHideParent);
+  }
+
+  showHideClicked(showHideParent) {
+    for (let ele of showHideParent.children) {
+
+      if (ele.classList.contains('showHide')) {
+        continue;
+      } else {
+          if (ele.classList.contains('hidden')) {
+          // ele.style.display = 'block';
+          ele.classList.toggle('hidden');
+          this.changeArrowDirection(showHideParent, 'arrow-expand', 'arrow-collapse');
+        } else {
+          ele.classList.toggle('hidden');
+          this.changeArrowDirection(showHideParent, 'arrow-collapse', 'arrow-expand');
+        }
+      }
+    }
+  }
+
+  changeArrowDirection(showHideParent, requiredDirection, removeDirection) {
+    const arrowsArr = document.querySelectorAll('.arrow');
+    for (const idx in arrowsArr) {
+      console.log(idx);
+      const arrowId = arrowsArr[idx].id.split('arrow-p')[1];
+      const projectId = showHideParent.id.split('p')[1];
+      // to expand or collapse project tasks arrow:
+      if (arrowId == projectId && arrowId != undefined) {
+        document.querySelector(`#arrow-p${arrowId}`).classList.remove(removeDirection);
+        document.querySelector(`#arrow-p${arrowId}`).classList.add(requiredDirection);
+        return;
+      // to expand or collapse all projects arrow:
+      } else if (showHideParent.id == 'left-bar') {
+        document.querySelector('#arrow-left-bar').classList.remove(removeDirection);
+        document.querySelector('#arrow-left-bar').classList.add(requiredDirection);
+        return;
+      }
+    }
+  }
+
+  hideSiblings(showHideParent) {
+    // Don't collapse expanded sub elements if project container is collapsed:
+    if (showHideParent.id == 'left-bar') {return};
+    const showHideDivArr = document.querySelectorAll('.showHide');
+    for (let showHideIdx = 0; showHideIdx <= showHideDivArr.length - 1; showHideIdx++) {
+      const ele = showHideDivArr[showHideIdx].parentElement
+      if (ele.id == 'left-bar' || ele.id == showHideParent.id) {
+        continue;
+      }
+      else {
+        ele.lastElementChild.classList.add('hidden');
+      }
+    }
+  }
 }
